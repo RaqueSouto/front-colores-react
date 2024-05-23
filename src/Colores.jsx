e8d464cba9e3cd4b6974b0eec8b3aa6d123c11f1
RaqueSouto <76965176+RaqueSouto@users.noreply.github.com>
2024-05-23 19:45:38 +0200

    front colores

---
+++ src/Colores.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react'
+import Formulario from "./Formulario"
+import Item from "./Item" 
+
+function Colores(){
+    
+    let [colores,setColores] = useState([])
+
+    useEffect(() => {
+        fetch("https://api-colores-mongo-l8n4.onrender.com/colores")
+        .then(respuesta => respuesta.json())
+        .then(colores => setColores(colores))
+    }, [])
+
+    function crearColor(color){
+
+        setColores([...colores,color])
+
+    }
+
+    function borrarColor(id){
+        setColores(colores.filter(color => color.id != id))
+    }
+
+    return(
+        <>
+        
+            <Formulario crearColor={crearColor} />
+            <ul>
+                { colores.map( ({id,r,g,b}) => <Item key={id} id={id} r={r} g={g} b={b} borrarColor={borrarColor} />) } 
+            </ul>
+
+        </>
+    )
+}
+
+export default Colores
